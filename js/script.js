199bd9f855c5303927aeee6d21ce7562dc3cb227
p9tno <plahotin_s@mail.ru>
2022-01-05 21:37:01 +0300

    add search

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ $(document).ready(function(){
         //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
             //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({scrollTop: top}, 1000);
     });
 });
 
